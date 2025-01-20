b591f4adda2625dcb29e8c0565732f9730d171d3
keaneconsulting <127808194+keaneconsulting@users.noreply.github.com>
2025-01-20 15:26:15 -0600

    G

---
+++ site.js
@@ -0,0 +1,8 @@
+const mainContentSection = document.querySelector('h2.active');
+mainContentSection.style.color = 'red';
+
+const fork = document.querySelector('footer div a');
+fork.style.textDecoration = 'underline';
+
+const fork2 = document.querySelector('#fork');
+fork2.style.textDecoration = 'underline';
